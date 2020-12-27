b805e478e3c983ed06e1de861eb310c76e4a09ca
sudesh sakthivel <sudesh.sakthivel@smallcase.com>
2020-12-27 19:37:13 +0530

    Added no image icon for unavailable resource

--- app/src/main/java/AppConstant.kt
+++ app/src/main/java/AppConstant.kt
@@ -1,7 +1,8 @@
 object AppConstant {
     const val BASE_URL = "http://www.omdbapi.com"
     /*
-    * NOTE: API key shouldn't be stored in your repo. However for conveniance of testing the project I am adding it here.
+    * NOTE: API key shouldn't be stored in your repo. However for convenience of testing the project I am adding it here.
     * */
     const val API_KEY = "8d1ebb86"
+    const val NO_POSTER = "N/A"
 }
--- app/src/main/java/com/example/openmoviedbswiggy/MovieViewHolder.kt
+++ app/src/main/java/com/example/openmoviedbswiggy/MovieViewHolder.kt
@@ -1,13 +1,30 @@
 package com.example.openmoviedbswiggy
 
+import AppConstant.NO_POSTER
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.openmoviedbswiggy.databinding.MovieGridItemBinding
 import com.example.openmoviedbswiggy.datamodel.MovieDataModel
 
 class MovieViewHolder(private val binding: MovieGridItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
+    private val noImageDrawable =
+        ContextCompat.getDrawable(binding.root.context, R.drawable.ic_no_image)
 
     fun bind(movieDataModel: MovieDataModel) {
         binding.titleTextView.text = movieDataModel.title
+        if (movieDataModel.posterImage == NO_POSTER) {
+            binding.movieThumbnailImageView.apply {
+                scaleType = ImageView.ScaleType.CENTER_INSIDE
+                setImageDrawable(noImageDrawable)
+            }
+        } else {
+            Glide.with(binding.root.context)
+                .load(movieDataModel.posterImage)
+                .centerCrop()
+                .into(binding.movieThumbnailImageView)
+        }
     }
 }
