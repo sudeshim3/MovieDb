3d8e399b00805609e23b389489140c9907229c06
sudesh sakthivel <sudesh.sakthivel@smallcase.com>
2020-12-30 19:21:15 +0530

    Added layout design for list items

--- app/src/main/java/com/example/openmoviedbswiggy/MovieRowViewHolder.kt
+++ app/src/main/java/com/example/openmoviedbswiggy/MovieRowViewHolder.kt
@@ -1,7 +1,10 @@
 package com.example.openmoviedbswiggy
 
+import AppConstant.NO_POSTER
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.openmoviedbswiggy.databinding.MovieRowItemBinding
 import com.example.openmoviedbswiggy.datamodel.MovieDataModel
 
@@ -11,9 +14,10 @@ class MovieRowViewHolder(private val binding: MovieRowItemBinding) :
         ContextCompat.getDrawable(binding.root.context, R.drawable.ic_no_image)
 
     fun bind(movieDataModel: MovieDataModel) {
-        binding.title.text = movieDataModel.title
-        /*if (movieDataModel.posterImage == NO_POSTER) {
-            binding.movieThumbnailImageView.apply {
+        binding.titleTextView.text = movieDataModel.title
+        binding.subtitleTextView.text = movieDataModel.year
+        if (movieDataModel.posterImage == NO_POSTER) {
+            binding.movieImage.apply {
                 scaleType = ImageView.ScaleType.CENTER_INSIDE
                 setImageDrawable(noImageDrawable)
             }
@@ -21,7 +25,7 @@ class MovieRowViewHolder(private val binding: MovieRowItemBinding) :
             Glide.with(binding.root.context)
                 .load(movieDataModel.posterImage)
                 .centerCrop()
-                .into(binding.movieThumbnailImageView)
-        }*/
+                .into(binding.movieImage)
+        }
     }
 }
